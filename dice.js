f6f5a27da4140da5d17dc4d4db2d0f8a6f26fef3
Pankaj kumar Pandey <69034032+pank1999@users.noreply.github.com>
2021-05-19 11:25:31 +0530

    updated dice.js

--- dice.js
+++ dice.js
@@ -1,12 +1,12 @@
 
 var randomNumber=Math.floor(Math.random()*6)+1;
 
-document.querySelectorAll("img")[0].setAttribute("src","/images/dice"+randomNumber+".png");
+document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber+".png");
 
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
 
-document.querySelectorAll('img')[1].setAttribute("src","/images/dice"+randomNumber2+".png");
+document.querySelectorAll('img')[1].setAttribute("src","images/dice"+randomNumber2+".png");
 
 if(randomNumber>randomNumber2){
     document.querySelector("h1").innerHTML="Player 1 win";
