03d6902e00c69b2615c3508274b5fe755661f557
Pankaj kumar Pandey <69034032+pank1999@users.noreply.github.com>
2021-05-19 10:57:50 +0530

    first commit

---
+++ dice.js
@@ -0,0 +1,19 @@
+
+var randomNumber=Math.floor(Math.random()*6)+1;
+
+document.querySelectorAll("img")[0].setAttribute("src","/images/dice"+randomNumber+".png");
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+document.querySelectorAll('img')[1].setAttribute("src","/images/dice"+randomNumber2+".png");
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 win";
+}
+else if(randomNumber<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 win";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
